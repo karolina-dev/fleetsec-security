2d52cd4d780186d71baaa2b3859d4c85fa4131a0
KAROLINA SEGURA PERDOMO <karolinaseguradev@gmail.com>
2026-09-24 22:01:05 -0500

    feat(terraform): habilitar estandares de Security Hub

--- modules/security-baseline/main.tf
+++ modules/security-baseline/main.tf
@@ -1137,6 +1137,16 @@ resource "aws_securityhub_account" "fleetsec" {
   ]
 }
 
+resource "aws_securityhub_standards_subscription" "cis_aws_foundations_v140" {
+  depends_on    = [aws_securityhub_account.fleetsec]
+  standards_arn = "arn:aws:securityhub:${var.aws_region}::standards/cis-aws-foundations-benchmark/v/1.4.0"
+}
+
+resource "aws_securityhub_standards_subscription" "aws_foundational_security_best_practices" {
+  depends_on    = [aws_securityhub_account.fleetsec]
+  standards_arn = "arn:aws:securityhub:${var.aws_region}::standards/aws-foundational-security-best-practices/v/1.0.0"
+}
+
 # ---------------------------------------------------------
 # AWS WAF
 # ---------------------------------------------------------

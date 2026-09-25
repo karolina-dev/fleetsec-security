dfd4ea2f1456d21ff51aa9aa32eae6ba00d3d8ae
KAROLINA SEGURA PERDOMO <karolinaseguradev@gmail.com>
2026-09-24 20:46:50 -0500

    fix(terraform): declarar variables del security baseline

--- modules/security-baseline/variables.tf
+++ modules/security-baseline/variables.tf
@@ -21,3 +21,39 @@ variable "admin_cidr" {
   type        = string
   default     = "10.0.0.0/16"
 }
+
+variable "vpc_cidr" {
+  description = "CIDR for the FleetSec VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "availability_zones" {
+  description = "Availability Zones for the FleetSec VPC"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
+}
+
+variable "public_subnet_cidrs" {
+  description = "Public subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "app_subnet_cidrs" {
+  description = "Application subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.11.0/24", "10.0.12.0/24"]
+}
+
+variable "data_subnet_cidrs" {
+  description = "Data subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.21.0/24", "10.0.22.0/24"]
+}
+
+variable "aws_account_id" {
+  description = "Placeholder AWS account ID for offline Terraform plan"
+  type        = string
+  default     = "123456789012"
+}
